3790fd958faad730c366240770cbb08fc38260f3
mdrodelo <66499864+mdrodelo@users.noreply.github.com>
2024-03-23 17:19:14 -0400

    superuser and admin fixes

--- backend/heatmap/admin.py
+++ backend/heatmap/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from heatmap.models import GameData, PlayerMovement
 # Register your models here.
+admin.site.register(GameData)
+admin.site.register(PlayerMovement)
